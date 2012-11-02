dc2b0585ea7f896489b3542c0551b86a320b9f85
sursrc <sursrc@gmail.com>
2012-11-02 14:32:14 +0800

    cancel and done button

---
+++ BirdWatching/AddSightingViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddSightingViewController.h
+//  BirdWatching
+//
+//  Created by Joseph Steve on 12-11-2.
+//  Copyright (c) 2012年 Joseph Steve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddSightingViewController : UITableViewController
+
+@end
